76f0b6fb372a7c410498494e052df671b2b815c7
ohmomar <ohmomar>
2023-09-27 09:48:45 +0000

    widget commit

---
+++ widget.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+dbutils.help()
+
+# COMMAND ----------
+
+dbutils.widgets.help()
+
+# COMMAND ----------
+
+dbutils.widgets.combobox(name="database",defaultValue="default",choices=["default","json","sample","test"],label="databases")
+
+# COMMAND ----------
+
+dbutils.widgets.get("database")
+
+# COMMAND ----------
+
+
